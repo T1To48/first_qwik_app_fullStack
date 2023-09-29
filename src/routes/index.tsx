78a4a5354b1ee12d75cea9d38df73a79b2cf54da
T1to48 <tofek.98@gmail.com>
2023-09-29 15:57:21 +0300

    configuration updates

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,35 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { useAuthSession, useAuthSignin, useAuthSignout } from '~/routes/plugin@auth';
+import { routeLoader$,Link } from "@builder.io/qwik-city";
+import prismaClient from "~/shared_lib/prismaClient";
+
+export const  useCategories=routeLoader$(async()=>{
+  const categories=await prismaClient.category.findMany();
+  return categories
+})
 
 export default component$(() => {
-  const session = useAuthSession();
-  
+  const categories=useCategories()
   return (
-    <>
-    <div>Email: {session.value?.user?.email}</div>
-    </>
+    <div class="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    {categories.value?.map((category) => (
+      <Link href={`/categories/${category.id}`} key={category.id}>
+        <div class="p-2">
+          <div class="card w-full glass">
+            <figure>
+              <img src={`/${category.image}`}  alt={category.name} width="100%" height="100%" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">{category.name}</h2>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Vote now!</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    ))}
+  </div>
   );
 });
 
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,11 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
+import { routeLoader$, Link, useLocation } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
 import { Header } from "~/components/Header";
 
 import styles from "./styles.css?inline";
+import prismaClient from "~/shared_lib/prismaClient";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -17,19 +18,36 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
 
+export const useCategories = routeLoader$(async () => {
+  const categories = await prismaClient.category.findMany();
+  return categories
+})
+const navItem="p-2";
+const currentNavItem=`${navItem} bg-gray-300 text-black rounded-md font-bold`
 export default component$(() => {
+  const categories = useCategories();
+
   useStyles$(styles);
+  const location = useLocation();
+
   return (
     <>
       <Header />
-      <main>
-        <Slot />
+      <main class="p-2">
+        <div class="md:grid md:grid-cols-[20%_80%] gap-3">
+          <div class="flex flex-col">
+            <Link href="/" class={location.url.pathname==="/"?currentNavItem:navItem} ><div>Home</div></Link>
+            {categories.value.map((category) => (
+              <Link href={`/categories/${category.id}`} key={category.id} class={location.url.pathname===`/categories/${category.id}`?currentNavItem:navItem}>
+                <div>{category.name}</div>
+              </Link>
+            ))}
+          </div>
+          <div>
+            <Slot />
+          </div>
+        </div>
       </main>
     </>
   );
